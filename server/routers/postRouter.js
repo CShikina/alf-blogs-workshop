5c7ff8dfa8d7410fcf03908862363daa84ac11eb
Shikina Cabral <130032168+CShikina@users.noreply.github.com>
2024-04-15 01:09:13 +0800

    Backend Workshop

---
+++ server/routers/postRouter.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const upload = require('../middleware/uploadMiddleware')
+const router = express.Router()
+
+const {
+    getAllPosts,
+    showPost,
+    createPost,
+    updatePost,
+    deletePost
+
+} = require('../controllers/postController')
+
+router.get('/', getAllPosts);
+router.get('/:id', showPost);
+router.post('/', upload.single('cover_photo'), createPost);
+router.put('/:id', upload.single('cover_photo'), updatePost); // patch can also be used, depends on updating is handled
+router.delete('/:id', deletePost);
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const connectDb = require('./config/db');
+const { errorHandler } = require('./middleware/errorMiddleware')
+
+//connect to database
+connectDb();
+
+app.use(express.static('public'))
+
+app.get('/AWS', (req, res) => {
+    res.status(200).json({ message: 'Hello AWSCC'});
+})
+
+//post routes:
+const postRouter = require('./routers/postRouter')
+app.use('/posts', postRouter)
+
+// Use Error Middleware
+app.use(errorHandler)
+
+app.listen(8080, () => {
+    console.log('Server is running in port 8080');
+})
+
